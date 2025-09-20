f2812b21ea09c07f8b677594c67b8b79e184b34b
Yunyao Yao <yunyao.yao@petcircle.com.au>
2025-09-20 20:10:18 +1000

    implemented function getArnieQuotes

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,17 @@
-const { httpGet } = require('./mock-http-interface');
+const { httpGet } = require("./mock-http-interface");
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  return Promise.all(
+    urls.map(async (url) => {
+      const response = await httpGet(url);
+      const parsedBody = JSON.parse(response.body);
+      if (response.status === 200) {
+        return { "Arnie Quote": parsedBody.message };
+      } else {
+        return { FAILURE: parsedBody.message };
+      }
+    })
+  );
 };
 
 module.exports = {
